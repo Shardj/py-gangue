0673ca2b687d1d72b580ae14ea2ab90664c16117
George Appleton <social@georgeappleton.co.uk>
2018-08-02 16:10:55 +0100

    added ascii art printing project name read from new config file

--- gangue.py
+++ gangue.py
@@ -1,3 +1,9 @@
 # Entry point
+
+import pyfiglet
+from app.configs.local import projectName
+
+print(pyfiglet.figlet_format(projectName, font = "slant"))
+
 import core.packages.router.route_manager as route_manager
 route_manager.RouteManger()
